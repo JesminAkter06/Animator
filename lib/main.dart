3fabe519335836879c2e877d9e2ed740fad48779
Jesmin Akter <jesmin143215@gmail.com>
2022-01-28 01:00:46 +0600

    commit this is first commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Animator() ,
+    );
+  }
+}
+class Animator extends StatefulWidget {
+  const Animator({Key? key}) : super(key: key);
+  @override
+  _AnimatorState createState() => _AnimatorState();
+}
+class _AnimatorState extends State<Animator> {
+  bool _value= false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+    child:Icon(Icons.play_arrow,size:55,color:Colors.purpleAccent),
+    onPressed: (){
+      setState(() {
+        _value=!_value;
+      });
+    }),
+    body:Center(
+        child: AnimatedContainer(
+    height: _value==false?50:600,
+    width: _value==false?30:500,
+    color: Colors.black,
+    child: CircleAvatar(
+      radius: 10,
+    backgroundColor: Colors.pinkAccent,
+    child: CircleAvatar(
+      radius: 230,
+    backgroundImage:AssetImage('images/04.jpg') ,
+    ),
+    ),
+    duration: Duration(
+    seconds: 5,
+    ),
+        ),
+
+    ),
+    );
+
+  }
+}
